ba2cbeb9364312616dffa9a8354ecd04bc0e13db
rini001 <debnathrenaissance95@gmail.com>
2025-05-14 18:49:23 +0530

    changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // or your backend URL
+  baseURL: 'https://bml-server-kv1a.onrender.com', // or your backend URL
   withCredentials: true,
 });
 
